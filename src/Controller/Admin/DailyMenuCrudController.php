a55ac816bfb128e56774e8415a47ec1ae3eafd95
dany rose <danyrose1995@gmail.com>
2025-02-06 18:29:56 +0100

    feat: enhance DailyMenuCrudController with locals

--- src/Controller/Admin/DailyMenuCrudController.php
+++ src/Controller/Admin/DailyMenuCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\DailyMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -17,11 +18,21 @@ class DailyMenuCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextEditorField::new('content', 'Menu du jour')
+            TextEditorField::new('content', 'Contenu')
                 ->onlyOnForms(),
-            TextField::new('content', 'Menu du jour')
+            TextField::new('content', 'Contenu')
                 ->onlyOnIndex()
                 ->renderAsHtml(),
         ];
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Menu du jour')
+            ->setEntityLabelInPlural('Menus du jour')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Liste des %entity_label_plural%')
+            ->setPageTitle(Crud::PAGE_EDIT, 'Modifier un %entity_label_singular%')
+            ->setPageTitle(Crud::PAGE_NEW, 'Ajouter un %entity_label_singular%');
     }
 }
